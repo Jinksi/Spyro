461f3e4cbb5a53bfc649c59a4d5a34a963db401c
Jinksi <ericjinks@gmail.com>
2016-09-16 07:04:56 +1000

    Add Babelify && es6 transform

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var uglify = require('gulp-uglify')
 var buffer = require('vinyl-buffer')
 var source = require('vinyl-source-stream')
 var gutil = require('gulp-util')
+var babelify = require("babelify")
 
 //metalsmith
 var metalsmith = require('gulp-metalsmith')
@@ -99,7 +100,7 @@ gulp.task('scripts', function() {
     entries: './js/main.js',
     debug: true,
     sourceMaps: true
-  })
+  }).transform("babelify", {presets: ["es2015"]})
   return b.bundle()
     .on('error', function(err) {
       bs.notify(err.message, 3000)
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 'use strict'
+
+const double = num => {
+  return num * 2
+}
+
+console.log(double(2))
